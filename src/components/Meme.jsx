a95d323f425dbc7b6e868d8e91b06b0fcd4c1344
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-03-26 22:32:57 +0100

    added image button function

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import memesData from "../assets/memesData";
 
 export default function Meme() {
-    return (
-        <section className="form--section">
-            <div className="form" action="">
-                <input className="form--input" placeholder="Top text..." type="text" />
-                <input className="form--input" type="text" placeholder="Bottom text..." />
-                <button className="form--button">Get a new meme image</button>
-            </div>
-        </section>
-    )
+  const [memeImage, setMemeImage] = React.useState("");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
+
+  return (
+    <section className="form--section">
+      <div className="form" action="">
+        <input className="form--input" placeholder="Top text..." type="text" />
+        <input
+          className="form--input"
+          type="text"
+          placeholder="Bottom text..."
+        />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image
+        </button>
+      </div>
+      <div className="meme--image-div">
+        <img
+          className="meme--image"
+          src={memeImage}
+          alt="meme image template"
+        />
+      </div>
+    </section>
+  );
 }
